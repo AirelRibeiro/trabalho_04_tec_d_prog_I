ecd9290e966ddec5fe0a47d34c232db8bd78aae8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-09-07 00:55:52 -0300

    feat: inicia padronização
    -- nome das colunas em snake_case, sem acentos ou caracteres especiis

--- cleaning.py
+++ cleaning.py
@@ -4,3 +4,14 @@ escolas_df = pd.read_csv("data/escolas.csv")
 subprefeituras_df = pd.read_csv("data/subprefeituras.csv")
 material_didatico_df = pd.read_csv("data/material_didatico.csv")
 
+# Padronização dos dados
+
+# Converte nomes de colunas para snake_case
+escolas_df.columns = [unidecode(col.lower()) for col in escolas_df.columns]
+subprefeituras_df.columns = [
+    unidecode(col.lower()) for col in subprefeituras_df.columns
+]
+material_didatico_df.columns = [
+    unidecode(col.lower()) for col in material_didatico_df.columns
+]
+
