bc69cb944cc7a2041013dbd0409c19fd0b6ef395
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-09-07 00:55:09 -0300

    feat: lê dados e cri dataframes

---
+++ cleaning.py
@@ -0,0 +1,6 @@
+import pandas as pd
+# Lê dos arquivos CSV
+escolas_df = pd.read_csv("data/escolas.csv")
+subprefeituras_df = pd.read_csv("data/subprefeituras.csv")
+material_didatico_df = pd.read_csv("data/material_didatico.csv")
+
